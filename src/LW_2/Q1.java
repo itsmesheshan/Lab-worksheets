8a51731b6c30a10a5201cc0853ab0acccb982c84
itsmesheshan <chanodsheshan4@gmail.com>
2025-09-28 19:24:37 +0530

    LW_2 questions

---
+++ src/LW_2/Q1.java
@@ -0,0 +1,15 @@
+package LW_2;
+
+public class Q1 {
+
+
+    public static void main(String[] args) {
+        for (int i=10;i<=49;i++){
+            System.out.print(i+" ");
+            if(i%10==9){
+                System.out.println();
+
+            }
+
+        }    }
+}
